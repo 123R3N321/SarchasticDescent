2f5167b1937b15daf5680a1f84dfa5c371308afd
123R3N321 <jr5887@nyu.edu>
2024-04-25 21:34:06 -0400

    Complete Preprocessing

--- Backend/FeatureSpam.py
+++ Backend/FeatureSpam.py
@@ -27,6 +27,22 @@ def countCurses(text):
 
 ##################################################################
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from collections import Counter
+from num2words import num2words
+
+import nltk
+import os
+import string
+import numpy as np
+import copy
+import pandas as pd
+import pickle
+import re
+import math
+
 '''
 Now handle embedding, start with basic term freq-inv docu freq (tf-idf)
 
@@ -37,8 +53,99 @@ Now handle embedding, start with basic term freq-inv docu freq (tf-idf)
 # necessary for tokenization
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
+
+#preprocessing
+def convert_lower_case(data):
+    return np.char.lower(data)
+
+def remove_stop_words(data):
+    stop_words = stopwords.words('english')
+    words = word_tokenize(str(data))
+    new_text = ""
+    for w in words:
+        if w not in stop_words and len(w) > 1:
+            new_text = new_text + " " + w
+    return new_text
+
+def remove_punctuation(data):
+    symbols = "!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
+    for i in range(len(symbols)):
+        data = np.char.replace(data, symbols[i], ' ')
+        data = np.char.replace(data, "  ", " ")
+    data = np.char.replace(data, ',', '')
+    return data
+
+def remove_apostrophe(data):
+    return np.char.replace(data, "'", "")
 
 
+def stemming(data):
+    stemmer = PorterStemmer()
+
+    tokens = word_tokenize(str(data))
+    new_text = ""
+    for w in tokens:
+        new_text = new_text + " " + stemmer.stem(w)
+    return new_text
+
+def convert_numbers(data):
+    tokens = word_tokenize(str(data))
+    new_text = ""
+    for w in tokens:
+        try:
+            w = num2words(int(w))
+        except:
+            a = 0
+        new_text = new_text + " " + w
+    new_text = np.char.replace(new_text, "-", " ")
+    return new_text
+
+def preprocess(data):
+    data = convert_lower_case(data)
+    data = remove_punctuation(data) #remove comma seperately
+    data = remove_apostrophe(data)
+    data = remove_stop_words(data)
+    data = convert_numbers(data)
+    data = stemming(data)
+    data = remove_punctuation(data)
+    data = convert_numbers(data)
+    data = stemming(data) #needed again as we need to stem the words
+    data = remove_punctuation(data) #needed again as num2word is giving few hypens and commas fourty-one
+    data = remove_stop_words(data) #needed again as num2word is giving stop words 101 - one hundred and one
+    return data
+
+'''
+tentative test dataset dimension (1000,9) exclusing y val
+features [0] and [8] do standard embedding (they are comment and parent post text)
+feature [1], is username, tentatively do profanity check
+'''
+
+
+'''
+first, need to scan all comments at features [0], [1], and [8] tp see recurrence of curse (separate)
+Note we only care if docu contains, not count in each
+assume data passed in is a simple column vector
+'''
+def crossDocOccurrence(data):
+    count = 0
+    for each in data:
+        if flagCurses(each):
+            count += 1
+    return count
+
+def idf(data):
+    resArr = np.zeros((data.shape[0],))
+    count = crossDocOccurrence(data)
+    for i in range(resArr.shape[0]):
+        resArr[i] = np.log(countCurses(data[i]))/count
+    return resArr
+
+def vecProcess(data):
+    resArr = np.zeros((data.shape[0],))
+    for i in range(resArr.shape[0]):
+        resArr[i] = preprocess(data[i])
+    return resArr
 
 
 if __name__ == "__main__":
@@ -49,3 +156,5 @@ if __name__ == "__main__":
     print(countCurses(" 'Maybe I never saw a camel, but I know a camel's cunt when I smell one' --Arya Stark"))
     print(countCurses("Once I've seen pranks on BG3: Arsetarion, Shadowfart, pokemon muk backward is cum, etc..."))
     print(countCurses("ok I guess this ass algo is not as fucking good as I freagging hoped for, son of a bitch."))
+
+    print(preprocess("Hello, this a the World!"))
